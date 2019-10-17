7eeddeb5fed85f6d1f0ec5fb3522c8ed232b4d3f
Scott Hankinson <scott.hankinson@slalom.com>
2019-10-17 16:07:05 -0400

    Updating to be a totally different deck...maybe tag it out?

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync . s3://www.slalom-serverless-demo.com --exclude ".git*" --exclude .DS_Store
+aws s3 sync . s3://www.slalom-serverless-demo.com --exclude ".git*" --exclude .DS_Store --delete
